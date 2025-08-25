4fe7c02de0d2d70d8f472e7ea447ae4dc1e0fd6b
grahsp <pontus.grahsp@gmail.com>
2025-08-25 16:17:47 +0200

    test: add tests for RsaSigningCredentialsStrategy

---
+++ OpenAuth.Infrastructure.Tests/Security/Signing/RsaSigningCredentialsStrategyTests.cs
@@ -0,0 +1,39 @@
+using System.Security.Cryptography;
+using OpenAuth.Domain.Entities;
+using OpenAuth.Domain.Enums;
+using OpenAuth.Infrastructure.Security.Signing;
+
+namespace OpenAuth.Infrastructure.Tests.Security.Signing;
+
+public class RsaSigningCredentialsStrategyTests
+{
+    private static SigningKey GenerateRsaKey()
+    {
+        using var rsa = RSA.Create(2048);
+        var privatePem = PemEncoding.Write("PRIVATE KEY", rsa.ExportPkcs8PrivateKey());
+        var publicPem  = PemEncoding.Write("PUBLIC KEY", rsa.ExportSubjectPublicKeyInfo());
+
+        return SigningKey.CreateAsymmetric(SigningAlgorithm.Rsa, new string(publicPem), new string(privatePem));
+    }
+
+    [Fact]
+    public void Create_Throws_WhenAlgorithmMismatch()
+    {
+        var key = SigningKey.CreateSymmetric(SigningAlgorithm.Hmac, "secret");
+        var strategy = new RsaSigningCredentialsStrategy();
+
+        Assert.Throws<InvalidOperationException>(() => strategy.GetSigningCredentials(key));
+    }
+
+    [Fact]
+    public void Create_ReturnsSigningCredentials_WithExpectedProperties()
+    {
+        var key = GenerateRsaKey();
+        var strategy = new RsaSigningCredentialsStrategy();
+
+        var signingCredentials = strategy.GetSigningCredentials(key);
+
+        Assert.Equal(SigningAlgorithm.Rsa, key.Algorithm);
+        Assert.Equal(key.KeyId.ToString(), signingCredentials.Key.KeyId);
+    }
+}
