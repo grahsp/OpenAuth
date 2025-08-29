7e61285dc9ec88eea6893a8bdbe4144aefc479ca
grahsp <pontus.grahsp@gmail.com>
2025-08-29 12:45:03 +0200

    fix: update ClientRepository methods Add and Remove into sync methods

--- OpenAuth.Application.Tests/Stubs/FakeClientRepository.cs
+++ OpenAuth.Application.Tests/Stubs/FakeClientRepository.cs
@@ -1,4 +1,4 @@
-using OpenAuth.Application.Repositories;
+using OpenAuth.Application.Abstractions;
 using OpenAuth.Domain.Entities;
 using OpenAuth.Domain.ValueObjects;
 
@@ -14,17 +14,11 @@ public class FakeClientRepository : IClientRepository
     public Task<Client?> GetByNameAsync(string name, CancellationToken ct = default) =>
         Task.FromResult(_store.Values.FirstOrDefault(c => c.Name == name));
 
-    public Task AddAsync(Client client, CancellationToken ct = default)
-    {
+    public void Add(Client client) =>
         _store[client.Id] = client;
-        return Task.CompletedTask;
-    }
 
-    public Task RemoveAsync(Client client, CancellationToken ct = default)
-    {
+    public void Remove(Client client) =>
         _store.Remove(client.Id);
-        return Task.CompletedTask;
-    }
 
     public IReadOnlyCollection<Client> Clients => _store.Values.ToList();
 }
--- OpenAuth.Application/Abstractions/IClientRepository.cs
+++ OpenAuth.Application/Abstractions/IClientRepository.cs
@@ -1,12 +1,12 @@
 using OpenAuth.Domain.Entities;
 using OpenAuth.Domain.ValueObjects;
 
-namespace OpenAuth.Application.Repositories;
+namespace OpenAuth.Application.Abstractions;
 
 public interface IClientRepository
 {
     Task<Client?> GetByIdAsync(ClientId id, CancellationToken cancellationToken = default);
     Task<Client?> GetByNameAsync(string name, CancellationToken cancellationToken = default);
-    Task AddAsync(Client client, CancellationToken cancellationToken = default);
-    Task RemoveAsync(Client client, CancellationToken cancellationToken = default);
+    void Add(Client client);
+    void Remove(Client client);
 }
