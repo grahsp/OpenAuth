58cec3eaeb293209714a8fb0cd1f6c081de34bf0
grahsp <pontus.grahsp@gmail.com>
2025-08-29 12:46:17 +0200

    feat: add implementation of ClientRepository using EF

---
+++ OpenAuth.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using OpenAuth.Application.Abstractions;
+using OpenAuth.Domain.Entities;
+using OpenAuth.Domain.ValueObjects;
+using OpenAuth.Infrastructure.Persistence;
+
+namespace OpenAuth.Infrastructure.Repositories;
+
+public class ClientRepository : IClientRepository
+{
+    private readonly AppDbContext _context;
+    public ClientRepository(AppDbContext context) => _context = context;
+    
+    public Task<Client?> GetByIdAsync(ClientId id, CancellationToken cancellationToken = default) =>
+        _context.Clients
+            .Include(x => x.Secrets)
+            .Include(x => x.SigningKeys)
+            .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+    public Task<Client?> GetByNameAsync(string name, CancellationToken cancellationToken = default) =>
+        _context.Clients
+            .Include(x => x.Secrets)
+            .Include(x => x.SigningKeys)
+            .FirstOrDefaultAsync(x => x.Name == name, cancellationToken);
+
+    public void Add(Client client) =>
+        _context.Clients.Add(client);
+
+    public void Remove(Client client) =>
+        _context.Clients.Remove(client);
+}
