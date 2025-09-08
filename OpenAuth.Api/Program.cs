2ad282989fe959994d4722a1316e5e2e7ff76640
grahsp <pontus.grahsp@gmail.com>
2025-09-08 19:22:25 +0200

    feat: register services to DI container

--- OpenAuth.Api/Program.cs
+++ OpenAuth.Api/Program.cs
@@ -1,3 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using OpenAuth.Application.Clients;
+using OpenAuth.Application.Security.Keys;
+using OpenAuth.Application.Security.Secrets;
+using OpenAuth.Application.Security.Signing;
+using OpenAuth.Application.Security.Tokens;
+using OpenAuth.Infrastructure.Persistence;
+using OpenAuth.Infrastructure.Repositories;
+using OpenAuth.Infrastructure.Security.Keys;
+using OpenAuth.Infrastructure.Security.Secrets;
+using OpenAuth.Infrastructure.Security.Signing;
+using OpenAuth.Infrastructure.Security.Tokens;
+
 namespace OpenAuth.Api;
 
 public class Program
@@ -10,6 +24,34 @@ public class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        // Database
+        builder.Services.AddDbContext<AppDbContext>(opts =>
+        {
+            opts.UseSqlServer("");
+        });
+
+        // Client Services
+        builder.Services.AddScoped<IClientRepository, ClientRepository>();
+        builder.Services.AddScoped<IClientSecretFactory, ClientSecretFactory>();
+        builder.Services.AddScoped<IClientSecretValidator, ClientSecretValidator>();
+        builder.Services.AddScoped<IClientService, ClientService>();
+        
+        // Client Secret
+        builder.Services.AddScoped<ISecretHasher, Pbkdf2Hasher>();
+        builder.Services.AddScoped<ISecretGenerator, SecretGenerator>();
+
+        // Signing Keys
+        builder.Services.AddScoped<ISigningKeyStrategy, HmacSigningKeyStrategy>();
+        builder.Services.AddScoped<ISigningKeyStrategy, RsaSigningKeyStrategy>();
+        builder.Services.AddScoped<ISigningKeyFactory, SigningKeyFactory>();
+
+        builder.Services.AddScoped<ISigningCredentialsStrategy, HmacSigningCredentialsStrategy>();
+        builder.Services.AddScoped<ISigningCredentialsStrategy, RsaSigningCredentialsStrategy>();
+        builder.Services.AddScoped<ISigningCredentialsFactory, SigningCredentialsFactory>();
+        
+        // Token Generator
+        builder.Services.AddSingleton(Options.Create("test-issuer"));
+        builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
         
 
         // Configure the HTTP request pipeline.
