ec5e25f6fc314701c47e1fcdcbd91079a2a838b4
grahsp <pontus.grahsp@gmail.com>
2025-09-09 07:48:05 +0200

    refactor: update Client controller

--- OpenAuth.Api/Controllers/ClientController.cs
+++ OpenAuth.Api/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using OpenAuth.Api.Dtos;
+using OpenAuth.Api.Mappers;
 using OpenAuth.Application.Clients;
 using OpenAuth.Domain.Entities;
 using OpenAuth.Domain.ValueObjects;
@@ -21,21 +22,32 @@ public class ClientController : ControllerBase
     public async Task<ActionResult<Client>> Get(Guid id)
     {
         var client = await _clientService.GetByIdAsync(new ClientId(id));
-        return client is null ? NotFound() : Ok(client);
+        if (client is null)
+            return NotFound();
+
+        var response = ClientMapper.ToResponse(client);
+        return Ok(response);
     }
     
     [HttpGet("{name}")]
     public async Task<ActionResult<Client>> Get(string name)
     {
         var client = await _clientService.GetByNameAsync(name);
-        return client is null ? NotFound() : Ok(client);
+        if (client is null)
+            return NotFound();
+        
+        var response = ClientMapper.ToResponse(client);
+        return Ok(response);
     }
     
     [HttpPost("create")]
-    public async Task<ActionResult<Client>> Create([FromBody] RegisterClientRequest request)
+    public async Task<ActionResult<RegisterClientResponse>> Create([FromBody] RegisterClientRequest request)
     {
         var client = await _clientService.RegisterAsync(request.Name);
-        return Ok(client);
+        var rawClientSecret = await _clientService.AddSecretAsync(client.Id);
+
+        var response = ClientMapper.ToResponse(client);
+        return Ok(new RegisterClientResponse(response, rawClientSecret));
     }
 
     [HttpDelete("{id:guid}")]
