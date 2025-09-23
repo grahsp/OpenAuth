3713e5e1d1a668e0117d4c27476b8f551c9fcf1a
grahsp <pontus.grahsp@gmail.com>
2025-09-23 09:39:54 +0200

    feat: extract RSA parameter export logic to reusable service
    - Add `KeyParameters` record and `IKeyParameterExporter` interface.
    - Implement `RsaKeyParameterExporter` for RSA parameter export.
    - Refactor `SigningKeyMapper` to use `KeyParameters` abstraction.
    - Update `JwksController` to inject and utilize `IKeyParameterExporter`.

--- OpenAuth.Api/Mappers/SigningKeyMapper.cs
+++ OpenAuth.Api/Mappers/SigningKeyMapper.cs
@@ -1,7 +1,6 @@
-using System.Security.Cryptography;
-using Microsoft.IdentityModel.Tokens;
 using OpenAuth.Api.Controllers;
 using OpenAuth.Api.Dtos;
+using OpenAuth.Application.SigningKeys;
 using OpenAuth.Domain.Entities;
 
 namespace OpenAuth.Api.Mappers;
@@ -18,33 +17,16 @@ public static class SigningKeyMapper
             key.RevokedAt
         );
 
-    public static Jwk ToJwk(SigningKey key)
+    public static Jwk ToJwk(SigningKey key, KeyParameters parameters)
     {
-        var (n, e) = ExportRsaParameters(key.PrivateKey);
-        
         return new Jwk
         {
             Kid = key.KeyId.Value.ToString(),
             Kty = "RSA",
             Alg = "RS256",
             Use = "sig",
-            N = n,
-            E = e,
+            N = parameters.N,
+            E = parameters.E,
         };
-    
-        (string N, string E) ExportRsaParameters(string privateKeyPem)
-        {
-            using var rsa = RSA.Create();
-            rsa.ImportFromPem(privateKeyPem);
-
-            var parameters = rsa.ExportParameters(false);
-            if (parameters.Modulus is null || parameters.Exponent is null)
-                throw new InvalidOperationException("RSA parameters are invalid: missing modulus or exponent.");
- 
-            return (
-                N: Base64UrlEncoder.Encode(parameters.Modulus),
-                E: Base64UrlEncoder.Encode(parameters.Exponent)
-            );
-        }
     }
 }
---
+++ OpenAuth.Application/SigningKeys/KeyParameters.cs
@@ -0,0 +1,3 @@
+namespace OpenAuth.Application.SigningKeys;
+
+public record KeyParameters(string N, string E);
