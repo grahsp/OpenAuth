83b6d76e4785fa18e0023462a252b1587129f847
grahsp <pontus.grahsp@gmail.com>
2025-10-04 17:50:22 +0200

    feat: add query specifications using extension methods for projecting secret into dto

--- OpenAuth.Infrastructure/Persistence/QuerySpecifications/SecretQuerySpecifications.cs
+++ OpenAuth.Infrastructure/Persistence/QuerySpecifications/SecretQuerySpecifications.cs
@@ -1,3 +1,4 @@
+using OpenAuth.Application.Dtos;
 using OpenAuth.Domain.Entities;
 
 namespace OpenAuth.Infrastructure.Persistence.QuerySpecifications;
@@ -8,4 +9,12 @@ public static class SecretQuerySpecifications
         this IQueryable<ClientSecret> query,
         DateTimeOffset now)
         => query.Where(k => k.RevokedAt == null && k.ExpiresAt > now);
+
+    public static IQueryable<SecretInfo> ToSecretInfo(this IQueryable<ClientSecret> query)
+        => query.Select(s => new SecretInfo(
+            s.Id,
+            s.CreatedAt,
+            s.ExpiresAt,
+            s.RevokedAt
+        ));
 }
