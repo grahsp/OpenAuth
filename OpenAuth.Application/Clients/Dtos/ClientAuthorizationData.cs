fe12ae6df0363239e03fb2b35c0bbfa81a5c99c0
grahsp <pontus.grahsp@gmail.com>
2025-10-11 09:09:38 +0200

    feat: allowing querying of clients authorization data

---
+++ OpenAuth.Application/Clients/Dtos/ClientAuthorizationData.cs
@@ -0,0 +1,12 @@
+using OpenAuth.Domain.Clients.Audiences;
+using OpenAuth.Domain.Clients.ValueObjects;
+
+namespace OpenAuth.Application.Clients.Dtos;
+
+public record ClientAuthorizationData(
+    ClientId ClientId,
+    bool RequirePkce,
+    GrantType[] GrantTypes,
+    RedirectUri[] RedirectUris,
+    Audience[] AllowedAudiences
+);
