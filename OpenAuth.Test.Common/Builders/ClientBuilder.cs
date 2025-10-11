25f552c2d60007f48ac0afee4ada09130072fd66
grahsp <pontus.grahsp@gmail.com>
2025-10-11 08:58:50 +0200

    test: update ClientBuilder with additional methods for audience, redirect uris and grant types

--- OpenAuth.Test.Common/Builders/ClientBuilder.cs
+++ OpenAuth.Test.Common/Builders/ClientBuilder.cs
@@ -1,4 +1,5 @@
 using OpenAuth.Domain.Clients;
+using OpenAuth.Domain.Clients.Audiences.ValueObjects;
 using OpenAuth.Domain.Clients.Secrets.ValueObjects;
 using OpenAuth.Domain.Clients.ValueObjects;
 
@@ -10,6 +11,10 @@ public class ClientBuilder
     private DateTimeOffset? _createdAt;
     
     private List<SecretHash> _secrets = [];
+    private Dictionary<string, string[]> _audiences = [];
+    private List<RedirectUri> _redirectUris = [];
+    private List<GrantType> _grantTypes = [];
+    
     
     public ClientBuilder WithName(string name)
     {
@@ -23,7 +28,7 @@ public class ClientBuilder
         return this;
     }
     
-    public ClientBuilder WithSecret(string secret)
+    public ClientBuilder WithSecret(string secret = "$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy")
     {
         _secrets.Add(new SecretHash(secret));
         return this;
@@ -32,6 +37,24 @@ public class ClientBuilder
     public ClientBuilder WithSecret(SecretHash secret)
     {
         _secrets.Add(secret);
+        return this;
+    }
+
+    public ClientBuilder WithRedirectUri(string uri = "http://example.com/callback")
+    {
+        _redirectUris.Add(RedirectUri.Create(uri));
+        return this;
+    }
+
+    public ClientBuilder WithGrantType(string grantType = GrantTypes.ClientCredentials)
+    {
+        _grantTypes.Add(GrantType.Create(grantType));
+        return this;
+    }
+
+    public ClientBuilder WithAudience(string audience, params string[] scopes)
+    {
+        _audiences.Add(audience, scopes);
         return this;
     }
 
@@ -50,6 +73,19 @@ public class ClientBuilder
         
         foreach (var secret in _secrets)
             client.AddSecret(secret, createdAt);
+
+        foreach (var (audience, scopes) in _audiences)
+        {
+            var audienceName = new AudienceName(audience);
+            client.AddAudience(audienceName, createdAt);
+            client.GrantScopes(audienceName, scopes.Select(s => new Scope(s)), createdAt);
+        }
+        
+        foreach (var redirectUri in _redirectUris)
+            client.AddRedirectUri(redirectUri, createdAt);
+        
+        foreach (var grantType in _grantTypes)
+            client.AddGrantType(grantType, createdAt);
 
         return client;
     }
