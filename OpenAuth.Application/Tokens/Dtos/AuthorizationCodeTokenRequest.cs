4b1cf895aaade24c9ef29b9bbd6e0600857942de
grahsp <pontus.grahsp@gmail.com>
2025-10-13 09:37:08 +0200

    test: add tests to ensure correct behavior of TokenService - add empty implementation of auth code flows to help in tests

---
+++ OpenAuth.Application/Tokens/Dtos/AuthorizationCodeTokenRequest.cs
@@ -0,0 +1,14 @@
+using OpenAuth.Domain.Clients.ValueObjects;
+
+namespace OpenAuth.Application.Tokens.Dtos;
+
+public record AuthorizationCodeTokenRequest : TokenRequest
+{
+    public override GrantType GrantType => GrantType.AuthorizationCode;
+    
+    public required string Code { get; init; }
+    public required RedirectUri RedirectUri { get; init; }
+    
+    public string? CodeVerifier { get; init; }
+    public string? ClientSecret { get; init; }
+}
