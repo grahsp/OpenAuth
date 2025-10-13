9939f0fc1bd77ed7cb617f1c2f475f15e203ad79
grahsp <pontus.grahsp@gmail.com>
2025-10-13 17:34:03 +0200

    test: add tests to ensure pkce is optional and works as intended in auth code flow

--- OpenAuth.Test.Unit/OAuth/AuthorizationCodeTokenIssuerTests.cs
+++ OpenAuth.Test.Unit/OAuth/AuthorizationCodeTokenIssuerTests.cs
@@ -3,6 +3,8 @@ using OpenAuth.Application.OAuth.Authorization.Interfaces;
 using OpenAuth.Application.Tokens.Dtos;
 using OpenAuth.Application.Tokens.Flows;
 using OpenAuth.Domain.AuthorizationGrant;
+using OpenAuth.Domain.AuthorizationGrant.Enums;
+using OpenAuth.Domain.AuthorizationGrant.ValueObjects;
 using OpenAuth.Domain.Clients.Audiences.ValueObjects;
 using OpenAuth.Domain.Clients.ValueObjects;
 
@@ -20,9 +22,6 @@ public class AuthorizationCodeTokenIssuerTests
     {
         _grantStore = Substitute.For<IAuthorizationGrantStore>();
         _sut = new AuthorizationCodeTokenIssuer(_grantStore);
-
-        _grantStore.GetAsync(DefaultGrant().Code)
-            .Returns(DefaultGrant());
     }
     
     private static AuthorizationCodeTokenRequest DefaultRequest()
@@ -33,9 +32,14 @@ public class AuthorizationCodeTokenIssuerTests
             RequestedAudience = new AudienceName("api.example.com"),
             RequestedScopes = [new Scope("read"), new Scope("write")]
         };
-    
-    private static AuthorizationGrant DefaultGrant()
-        => AuthorizationGrant.Create(
+
+    private static AuthorizationGrant DefaultGrant(string? codeChallenge = null)
+    {
+        var pkce = codeChallenge is not null
+            ? Pkce.Create(codeChallenge, CodeChallengeMethod.S256)
+            : null;
+        
+        return AuthorizationGrant.Create(
             "auth-code",
             GrantType.AuthorizationCode,
             DefaultClientId,
@@ -43,16 +47,38 @@ public class AuthorizationCodeTokenIssuerTests
             new AudienceName("api.example.com"),
             [new Scope("read"), new Scope("write")],
             DateTimeOffset.UtcNow,
-            null
+            pkce
         );
-    
+    }
+
+    private void SetupGrantStore(AuthorizationGrant grant)
+        => _grantStore.GetAsync(grant.Code).Returns(grant);
 
     [Fact]
-    public async Task IssueToken_WithValidGrant_ReturnsExpectedTokenContext()
+    public async Task IssueToken_WithValidGrant_WithoutPkce_ReturnsExpectedTokenContext()
     {
         // Arrange
-        var grant = DefaultGrant();
         var request = DefaultRequest();
+        var grant = DefaultGrant();
+        SetupGrantStore(grant);
+
+        // Act
+        var result = await _sut.IssueToken(request);
+
+        // Assert
+        Assert.Equal(grant.ClientId, result.ClientId);
+        Assert.Equal(grant.ClientId.ToString(), result.Subject);
+        Assert.Equal(grant.Audience, result.RequestedAudience);
+        Assert.Equal(grant.Scopes, result.RequestedScopes);
+    }
+    
+    [Fact]
+    public async Task IssueToken_WithValidGrant_WithPkce_ReturnsExpectedTokenContext()
+    {
+        // Arrange
+        var request = DefaultRequest() with { CodeVerifier = "code-challenge" };
+        var grant = DefaultGrant("code-challenge");
+        SetupGrantStore(grant);
 
         // Act
         var result = await _sut.IssueToken(request);
@@ -69,7 +95,6 @@ public class AuthorizationCodeTokenIssuerTests
     {
         // Arrange
         var request = DefaultRequest() with { Code = "invalid-code" };
-    
         _grantStore.GetAsync("invalid-code")
             .Returns((AuthorizationGrant?)null);
     
@@ -82,12 +107,11 @@ public class AuthorizationCodeTokenIssuerTests
     public async Task IssueToken_WhenGrantIsConsumed_ThrowsInvalidOperationException()
     {
         // Arrange
-        var grant = DefaultGrant();
-        grant.Consume();
-        
-        _grantStore.GetAsync(grant.Code).Returns(grant);
-
         var request = DefaultRequest();
+        var grant = DefaultGrant();
+        
+        grant.Consume();
+        SetupGrantStore(grant);
     
         // Act & Assert
         var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _sut.IssueToken(request));
@@ -99,6 +123,8 @@ public class AuthorizationCodeTokenIssuerTests
     {
         // Arrange
         var request = DefaultRequest() with { ClientId = ClientId.New() };
+        var grant = DefaultGrant();
+        SetupGrantStore(grant);
     
         // Act & Assert
         var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _sut.IssueToken(request));
@@ -110,9 +136,37 @@ public class AuthorizationCodeTokenIssuerTests
     {
         // Arrange
         var request = DefaultRequest() with { RedirectUri = RedirectUri.Create("https://invalid-uri.com/callback") };
+        var grant = DefaultGrant();
+        SetupGrantStore(grant);
     
         // Act & Assert
         var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _sut.IssueToken(request));
         Assert.Equal("Redirect URI mismatch.", ex.Message);
+    }
+
+    [Fact]
+    public async Task IssueToken_WithMissingCodeChallenge_WhenPkceRequired_ThrowsInvalidOperationException()
+    {
+        // Arrange
+        var request = DefaultRequest() with { CodeVerifier = null };
+        var grant = DefaultGrant("code-challenge");
+        SetupGrantStore(grant);
+        
+        // Act & Assert
+        var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _sut.IssueToken(request));
+        Assert.Equal("Invalid PKCE code verifier.", ex.Message);
+    }
+
+    [Fact]
+    public async Task IssueToken_WithInvalidCodeChallenge_WhenPkceRequired_ThrowsInvalidOperationException()
+    {
+        // Arrange
+        var request = DefaultRequest() with { CodeVerifier = "invalid-code-challenge" };
+        var grant = DefaultGrant("code-challenge");
+        SetupGrantStore(grant);
+        
+        // Act & Assert
+        var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _sut.IssueToken(request));
+        Assert.Equal("Invalid PKCE code verifier.", ex.Message);
     }
 }
