30b2b0976a53ac177163f790f8a89dffec9a4aef
grahsp <pontus.grahsp@gmail.com>
2025-10-13 17:34:35 +0200

    feat: implement AuthorizationCodeTokenIssuer

--- OpenAuth.Application/Tokens/Flows/AuthorizationCodeTokenIssuer.cs
+++ OpenAuth.Application/Tokens/Flows/AuthorizationCodeTokenIssuer.cs
@@ -1,6 +1,7 @@
 using OpenAuth.Application.OAuth.Authorization.Interfaces;
 using OpenAuth.Application.Tokens.Dtos;
 using OpenAuth.Domain.Clients.ValueObjects;
+using TokenContext = OpenAuth.Application.Tokens.Dtos.TokenContext;
 
 namespace OpenAuth.Application.Tokens.Flows;
 
@@ -17,6 +18,27 @@ public class AuthorizationCodeTokenIssuer : TokenIssuerBase<AuthorizationCodeTok
     
     protected override async Task<TokenContext> IssueToken(AuthorizationCodeTokenRequest request, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var grant = await _grantStore.GetAsync(request.Code)
+                    ?? throw new InvalidOperationException("Invalid authorization code.");
+        
+        if (grant.Consumed)
+            throw new InvalidOperationException("Authorization code has already been used.");
+
+        if (grant.ClientId != request.ClientId)
+            throw new InvalidOperationException("Client ID mismatch.");
+        
+        if (grant.RedirectUri != request.RedirectUri)
+            throw new InvalidOperationException("Redirect URI mismatch.");
+        
+        if (grant.Pkce is not null && !grant.Pkce.Matches(request.CodeVerifier))
+            throw new InvalidOperationException("Invalid PKCE code verifier.");
+        
+        
+        return new TokenContext(
+            grant.ClientId,
+            grant.ClientId.ToString(),
+            grant.Audience,
+            grant.Scopes
+        );
     }
 }
