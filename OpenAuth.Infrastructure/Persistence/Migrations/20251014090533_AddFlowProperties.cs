6c2470da049be38cf59096c57c7aadf22959777c
grahsp <pontus.grahsp@gmail.com>
2025-10-14 11:14:45 +0200

    chore: add migrations for added flow config properties of client

---
+++ OpenAuth.Infrastructure/Persistence/Migrations/20251014090533_AddFlowProperties.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenAuth.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFlowProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
