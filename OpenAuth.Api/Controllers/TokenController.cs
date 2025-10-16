4e86a35af5807deb2c34c35b0dab7c0d4facf8fa
grahsp <pontus.grahsp@gmail.com>
2025-10-16 14:26:36 +0200

    fix: update TokenController to contain test data

--- OpenAuth.Api/Controllers/TokenController.cs
+++ OpenAuth.Api/Controllers/TokenController.cs
@@ -1,8 +1,14 @@
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using OpenAuth.Api.Dtos;
-using OpenAuth.Application.Tokens;
+using OpenAuth.Application.OAuth.Authorization.Dtos;
+using OpenAuth.Application.OAuth.Authorization.Handlers;
 using OpenAuth.Application.Tokens.Dtos;
 using OpenAuth.Application.Tokens.Services;
+using OpenAuth.Domain.AuthorizationGrants.Enums;
+using OpenAuth.Domain.AuthorizationGrants.ValueObjects;
 using OpenAuth.Domain.Clients.Audiences.ValueObjects;
 using OpenAuth.Domain.Clients.ValueObjects;
 
@@ -13,24 +19,58 @@ namespace OpenAuth.Api.Controllers;
 public class TokenController : ControllerBase
 {
     private readonly ITokenService _tokenService;
+    private readonly IAuthorizationHandler _authorizationHandler;
     
-    public TokenController(ITokenService tokenService)
+    public TokenController(ITokenService tokenService, IAuthorizationHandler authorizationHandler)
     {
         _tokenService = tokenService;
+        _authorizationHandler = authorizationHandler;
     }
 
     
     [HttpPost("token")]
-    public async Task<ActionResult<TokenResponse>> IssueToken([FromBody] ClientCredentialsRequest request)
+    public async Task<ActionResult<TokenResponse>> IssueToken([FromBody] string code)
     {
-        var tokenRequest = new IssueTokenRequest(
-            new ClientId(Guid.Parse(request.ClientId)),
-            request.ClientSecret,
-            new AudienceName(request.Audience),
-            request.Scopes.Select(s => new Scope(s)).ToArray()
-        );
+        // Client Credentials
+        var ccRequest = new ClientCredentialsTokenRequest()
+        {
+            ClientId = new ClientId(Guid.Parse("c2b09d6b-e5cb-4154-a03f-7a824fc9c5df")),
+            ClientSecret = "68loWhVtCow-q3AzYy3RE7vd7tV94tZ9WtX1Mj9PEJA",
+            RequestedAudience = new AudienceName("api"),
+            RequestedScopes = [new Scope("read")]
+        };
+
+        // Authorization Code
+        var acRequest = new AuthorizationCodeTokenRequest()
+        {
+            ClientId = new ClientId(Guid.Parse("c2b09d6b-e5cb-4154-a03f-7a824fc9c5df")),
+            Code = code,
+            Subject = "Test-Subject",
+            RedirectUri = RedirectUri.Create("https://example.com/callback"),
+            RequestedAudience = new AudienceName("api"),
+            RequestedScopes = [new Scope("read")],
+            CodeVerifier = "this-is-a-secret-code"
+            // ClientSecret = "68loWhVtCow-q3AzYy3RE7vd7tV94tZ9WtX1Mj9PEJA",
+        };
         
-        var result = await _tokenService.IssueToken(tokenRequest);
+        var result = await _tokenService.IssueToken(ccRequest);
         return Ok(new TokenResponse(result.Token, result.TokenType, result.ExpiresIn));
+    }
+
+    [HttpPost("authorize")]
+    public async Task<ActionResult> Authorize()
+    {
+        var codeChallenge = Base64UrlEncoder.Encode(SHA256.HashData(Encoding.UTF8.GetBytes("this-is-a-secret-code")));
+        var request = new AuthorizationRequest(
+            new ClientId(Guid.Parse("c2b09d6b-e5cb-4154-a03f-7a824fc9c5df")),
+            GrantType.AuthorizationCode,
+            RedirectUri.Create("https://example.com/callback"),
+            new AudienceName("api"),
+            [new Scope("read")],
+            Pkce.Create(codeChallenge, CodeChallengeMethod.S256)
+        );
+
+        var result = await _authorizationHandler.AuthorizeAsync(request);
+        return Ok(result);
     }
 }
