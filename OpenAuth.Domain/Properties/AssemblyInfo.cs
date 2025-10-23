127e60e7be5fb010d1ca5d6610545cc4e18554a9
grahsp <pontus.grahsp@gmail.com>
2025-10-23 20:39:52 +0200

    feat: configure EF settings for User - add migrations - add integration tests for persistence

--- OpenAuth.Domain/Properties/AssemblyInfo.cs
+++ OpenAuth.Domain/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("OpenAuth.Test.Common")]
 [assembly: InternalsVisibleTo("OpenAuth.Test.Unit")]
+[assembly: InternalsVisibleTo("OpenAuth.Test.Integration")]
 [assembly: InternalsVisibleTo("OpenAuth.Application")]
