e650b126085991be3a526c4b29526114f678ec93
grahsp <pontus.grahsp@gmail.com>
2025-10-29 16:04:34 +0100

    refactor: implement Identity and replace user - remove old tests and services depending on domain user

--- OpenAuth.Domain/Users/User.cs
+++ OpenAuth.Domain/Users/User.cs
@@ -1,32 +1,8 @@
-using OpenAuth.Domain.Users.ValueObjects;
+using Microsoft.AspNetCore.Identity;
 
 namespace OpenAuth.Domain.Users;
 
-public class User
+public class User : IdentityUser<Guid>
 {
-    public UserId Id { get; private init; }
-    public string Username { get; private set; }
-    public Email Email { get; private set; }
-    public HashedPassword HashedPassword { get; private set; }
-    public bool IsActive { get; private set; }
-    public DateTimeOffset CreatedAt { get; private set; }
 
-    private User() { }
-
-    private User(string username, Email email, HashedPassword hashedPassword, DateTimeOffset utcNow)
-    {
-        ArgumentException.ThrowIfNullOrWhiteSpace(username);
-        
-        Id = UserId.New();
-        Username = username;
-        Email = email;
-        HashedPassword = hashedPassword;
-        IsActive = true;
-        CreatedAt = utcNow;
-    }
-
-    internal static User Create(string username, Email email, HashedPassword passwordHash, DateTimeOffset utcNow)
-    {
-        return new User(username, email, passwordHash, utcNow);
-    }
 }
