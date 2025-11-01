dffbae1b22c3b42e163ba07f9206028cc8ffd9c4
grahsp <pontus.grahsp@gmail.com>
2025-11-01 16:08:14 +0100

    refactor: update id value objects to inherit ICreate

--- OpenAuth.Domain/Clients/Audiences/ValueObjects/AudienceId.cs
+++ OpenAuth.Domain/Clients/Audiences/ValueObjects/AudienceId.cs
@@ -1,7 +1,31 @@
+using System.Diagnostics.CodeAnalysis;
+using OpenAuth.Domain.Shared.Interfaces;
+
 namespace OpenAuth.Domain.Clients.Audiences.ValueObjects;
 
-public record AudienceId(Guid Value)
+public record AudienceId(Guid Value) : ICreate<string, AudienceId>
 {
-    public static AudienceId New()
-        => new(Guid.NewGuid());
+    public static AudienceId New() => new(Guid.NewGuid());
+    
+    public static AudienceId Create(string value)
+    {
+        if (!TryCreate(value, out var id))
+            throw new ArgumentException($"Invalid client ID: {value}", nameof(value));
+
+        return id;
+    }
+
+    public static bool TryCreate(string value, [NotNullWhen(true)] out AudienceId? id)
+    {
+        if (!Guid.TryParse(value, out var guid) || guid == Guid.Empty)
+        {
+            id = null;
+            return false;
+        }
+        
+        id = new AudienceId(guid);
+        return true;
+    }
+    
+    public override string ToString() => Value.ToString();
 }
--- OpenAuth.Domain/Clients/Secrets/ValueObjects/SecretId.cs
+++ OpenAuth.Domain/Clients/Secrets/ValueObjects/SecretId.cs
@@ -1,7 +1,31 @@
+using System.Diagnostics.CodeAnalysis;
+using OpenAuth.Domain.Shared.Interfaces;
+
 namespace OpenAuth.Domain.Clients.Secrets.ValueObjects;
 
-public readonly record struct SecretId(Guid Value)
+public record SecretId(Guid Value) : ICreate<string, SecretId>
 {
     public static SecretId New() => new(Guid.NewGuid());
+    
+    public static SecretId Create(string value)
+    {
+        if (!TryCreate(value, out var id))
+            throw new ArgumentException($"Invalid client ID: {value}", nameof(value));
+
+        return id;
+    }
+
+    public static bool TryCreate(string value, [NotNullWhen(true)] out SecretId? id)
+    {
+        if (!Guid.TryParse(value, out var guid) || guid == Guid.Empty)
+        {
+            id = null;
+            return false;
+        }
+        
+        id = new SecretId(guid);
+        return true;
+    }
+    
     public override string ToString() => Value.ToString();
 }
