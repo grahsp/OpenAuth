64cf64c67120db0335dc326522f61881ae4f068d
grahsp <pontus.grahsp@gmail.com>
2025-11-02 20:25:14 +0100

    feat: update SecuritySettings with validation - add test builder -add tests

--- OpenAuth.Domain/Clients/ValueObjects/SecuritySettings.cs
+++ OpenAuth.Domain/Clients/ValueObjects/SecuritySettings.cs
@@ -3,22 +3,44 @@ using OpenAuth.Domain.Clients.Secrets;
 
 namespace OpenAuth.Domain.Clients.ValueObjects;
 
-public record SecuritySettings(
-    IReadOnlyCollection<Secret> Secrets,
-    TimeSpan? TokenLifetime)
+public record SecuritySettings
 {
-    public SecuritySettings AddSecrets(params Secret[] secrets)
-        => this with {
-            Secrets = Secrets
-                .Union(secrets)
-                .ToImmutableArray()
-        };
-
-    public SecuritySettings SetTokenLifetime(TimeSpan? lifetime)
+    public IReadOnlyCollection<Secret> Secrets;
+    public TimeSpan TokenLifetime;
+    
+    private TimeSpan DefaultTokenLifetime => TimeSpan.FromMinutes(15);
+    
+    public SecuritySettings(IEnumerable<Secret> secrets, TimeSpan? tokenLifetime)
     {
-        if (lifetime is not null && (lifetime <= TimeSpan.Zero || lifetime > TimeSpan.FromDays(30)))
-            throw new ArgumentOutOfRangeException(nameof(lifetime), "TokenLifetime must be positive and less than 30 days.");
-            
-        return this with { TokenLifetime = lifetime };
+        Secrets = ValidateSecrets(secrets);
+        TokenLifetime = ValidateLifetime(tokenLifetime ?? DefaultTokenLifetime);
+    }
+    
+    public SecuritySettings AddSecret(Secret secret)
+    {
+        var updatedSecrets = Secrets.Append(secret);
+        return this with { Secrets = ValidateSecrets(updatedSecrets) };
+    }
+
+    private IReadOnlyCollection<Secret> ValidateSecrets(IEnumerable<Secret> secrets)
+    {
+        var items = secrets.ToImmutableList();
+        
+        var distinct = items.DistinctBy(s => s.Id);
+        if (distinct.Count() != items.Count)
+            throw new ArgumentException("Duplicate secret IDs found.");
+
+        return items;
+    }
+
+    public SecuritySettings SetTokenLifetime(TimeSpan lifetime)
+        => this with { TokenLifetime = ValidateLifetime(lifetime)};
+
+    private TimeSpan ValidateLifetime(TimeSpan lifetime)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(lifetime, TimeSpan.Zero, nameof(lifetime));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(lifetime, TimeSpan.FromDays(30), nameof(lifetime));
+        
+        return lifetime;
     }
 }
---
+++ OpenAuth.Test.Common/Builders/SecuritySettingsBuilder.cs
@@ -0,0 +1,29 @@
+using OpenAuth.Domain.Clients.Secrets;
+using OpenAuth.Domain.Clients.ValueObjects;
+
+namespace OpenAuth.Test.Common.Builders;
+
+public class SecuritySettingsBuilder
+{
+    private IEnumerable<Secret> _secrets = [];
+    private TimeSpan? _lifetime;
+    
+    public SecuritySettingsBuilder WithSecrets(params Secret[] secrets)
+    {
+        _secrets = secrets;
+        return this;
+    }
+    
+    public SecuritySettingsBuilder WithTokenLifetime(TimeSpan lifetime)
+    {
+        _lifetime = lifetime;
+        return this;
+    }
+
+    public SecuritySettings Build()
+    {
+        var lifetime = _lifetime ?? TimeSpan.FromMinutes(10);
+        
+        return new SecuritySettings(_secrets, lifetime);
+    }
+}
