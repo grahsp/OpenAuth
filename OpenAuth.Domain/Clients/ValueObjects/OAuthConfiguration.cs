df3e3ff62632c7ea536cb3b43a2ec2bfccb2b80e
grahsp <pontus.grahsp@gmail.com>
2025-11-03 12:56:21 +0100

    refactor: OAuthConfiguration to use set methods instead of add and remove with improved validation - add test builder

--- OpenAuth.Domain/Clients/ValueObjects/OAuthConfiguration.cs
+++ OpenAuth.Domain/Clients/ValueObjects/OAuthConfiguration.cs
@@ -3,34 +3,90 @@ using OpenAuth.Domain.Clients.Audiences;
 
 namespace OpenAuth.Domain.Clients.ValueObjects;
 
-public record OAuthConfiguration(
-    IReadOnlyCollection<Audience> Audiences,
-    IReadOnlyCollection<GrantType> GrantTypes,
-    IReadOnlyCollection<RedirectUri> RedirectUris,
-    bool RequirePkce)
+public record OAuthConfiguration
 {
-    public OAuthConfiguration AddAudiences(params Audience[] audiences)
-        => this with {
-            Audiences = Audiences
-                .Union(audiences)
-                .ToImmutableArray()
-        };
+    public IReadOnlyCollection<Audience> Audiences { get; private init; }
+    public IReadOnlyCollection<GrantType> GrantTypes { get; private init; }
+    public IReadOnlyCollection<RedirectUri> RedirectUris { get; private init; }
+    public bool RequirePkce { get; private init; }
+    
+    public OAuthConfiguration(
+        IEnumerable<Audience> audiences,
+        IEnumerable<GrantType> grantTypes,
+        IEnumerable<RedirectUri> redirectUris,
+        bool requirePkce)
+    {
+        Audiences = audiences.ToImmutableList();
+        GrantTypes = grantTypes.ToImmutableList();
+        RedirectUris = redirectUris.ToImmutableList();
+        RequirePkce = requirePkce;
+    }
+    
+    public OAuthConfiguration SetAudiences(IEnumerable<Audience> audiences)
+    {
+        ArgumentNullException.ThrowIfNull(audiences);
 
-    public OAuthConfiguration AddGrantTypes(params GrantType[] grantTypes)
-        => this with {
-            GrantTypes = GrantTypes
-                .Union(grantTypes)
-                .ToImmutableArray()
-        };
+        var items = audiences.DistinctBy(a => a.Name).ToArray();
+        
+        if (items.Length == 0)
+            throw new InvalidOperationException("Client must have at least one audience.");
 
-    public OAuthConfiguration AddRedirectUris(params RedirectUri[] redirectUris)
-        => this with
-        {
-            RedirectUris = RedirectUris
-                .Union(redirectUris)
-                .ToImmutableArray()
-        };
+        if (Audiences.SequenceEqual(items))
+            return this;
+        
+        return this with { Audiences = items.ToImmutableList() };
+    }
+
+    public OAuthConfiguration SetGrantTypes(IEnumerable<GrantType> grantTypes)
+    {
+        ArgumentNullException.ThrowIfNull(grantTypes);
+        
+        var grants = grantTypes.Distinct().ToArray();
+        
+        if (grants.Length == 0)
+            throw new InvalidOperationException("Client must have at least one grant type.");
+
+        var allPublic= grants.All(gt => gt.SupportsPublicClient);
+        var allConfidential= grants.All(gt => !gt.SupportsPublicClient);
+        var includesAuthCode = grants.Contains(GrantType.AuthorizationCode);
+        
+        var isMixed = !allPublic && !allConfidential;
+        var isAuthCodePublic = includesAuthCode && RequirePkce;
+        
+        if (isMixed && !isAuthCodePublic)
+            throw new InvalidOperationException("Cannot mix public and confidential grant types.");
+        
+        var requiresRedirectUri = grants.Any(gt => gt.RequiresRedirectUri);
+        
+        if (requiresRedirectUri && RedirectUris.Count == 0)
+            throw new InvalidOperationException("Client must have at least one redirect URI.");
+        
+        return this with { GrantTypes = grants.ToImmutableList() };
+    }
+
+    public OAuthConfiguration SetRedirectUris(IEnumerable<RedirectUri> redirectUris)
+    {
+        ArgumentNullException.ThrowIfNull(redirectUris);
+        
+        var uris = redirectUris.Distinct().ToArray();
+        
+        if (uris.Length == 0 && GrantTypes.Any(gt => gt.RequiresRedirectUri))
+            throw new InvalidOperationException("Client must have at least one redirect URI.");
+        
+        return this with { RedirectUris = uris.ToImmutableList() };
+    }
 
     public OAuthConfiguration SetRequirePkce(bool requirePkce)
-        => this with { RequirePkce = requirePkce };
+    {
+        var allPublic = GrantTypes.All(gt => gt.SupportsPublicClient);
+        var includesAuthCode = GrantTypes.Contains(GrantType.AuthorizationCode);
+        
+        if (!requirePkce && allPublic && includesAuthCode)
+            throw new InvalidOperationException("Cannot disable PKCE for public clients.");
+        
+        if (RequirePkce == requirePkce)
+            return this;
+        
+        return this with { RequirePkce = requirePkce };
+    }
 }
---
+++ OpenAuth.Test.Common/Builders/OAuthConfigurationBuilder.cs
@@ -0,0 +1,51 @@
+using OpenAuth.Domain.Clients.Audiences;
+using OpenAuth.Domain.Clients.Audiences.ValueObjects;
+using OpenAuth.Domain.Clients.ValueObjects;
+
+namespace OpenAuth.Test.Common.Builders;
+
+public class OAuthConfigurationBuilder
+{
+    private IEnumerable<Audience>? _audiences;
+    private IEnumerable<GrantType>? _grantTypes;
+    private IEnumerable<RedirectUri>? _redirectUris;
+    private bool? _requirePkce;
+
+    public OAuthConfigurationBuilder WithAudiences(params Audience[] audiences)
+    {
+        _audiences = audiences;
+        return this;
+    }
+    
+    public OAuthConfigurationBuilder WithGrantTypes(params GrantType[] grantTypes)
+    {
+        _grantTypes = grantTypes;
+        return this;
+    }
+    
+    public OAuthConfigurationBuilder WithRedirectUris(params RedirectUri[] redirectUris)
+    {
+        _redirectUris = redirectUris;
+        return this;
+    }
+    
+    public OAuthConfigurationBuilder WithRequirePkce(bool requirePkce)
+    {
+        _requirePkce = requirePkce;
+        return this;
+    }
+
+    public OAuthConfiguration Build()
+    {
+        var audiences = _audiences
+                        ?? [Audience.Create(AudienceName.Create("api"), DateTimeOffset.UtcNow)];
+        var grantTypes = _grantTypes
+            ?? [GrantType.AuthorizationCode];
+        var redirectUris = _redirectUris
+            ?? [RedirectUri.Create("http://example.com/callback")];
+        var requirePkce = _requirePkce
+            ?? true;
+        
+        return new OAuthConfiguration(audiences, grantTypes, redirectUris, requirePkce);
+    }
+}
