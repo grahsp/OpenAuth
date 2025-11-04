95428a5b811709fd81fa161f958645877ced399c
grahsp <pontus.grahsp@gmail.com>
2025-11-04 13:13:51 +0100

    test: add unit tests and override equality in NewAudience

--- OpenAuth.Domain/Clients/ValueObjects/NewAudience.cs
+++ OpenAuth.Domain/Clients/ValueObjects/NewAudience.cs
@@ -2,11 +2,22 @@ using OpenAuth.Domain.Clients.Audiences.ValueObjects;
 
 namespace OpenAuth.Domain.Clients.ValueObjects;
 
-public record NewAudience(AudienceName Name, ScopeCollection Scopes)
+public sealed record NewAudience(AudienceName Name, ScopeCollection Scopes) : IEquatable<NewAudience>
 {
     public NewAudience SetName(AudienceName name)
         => this with { Name = name };
     
     public NewAudience SetScopes(ScopeCollection scopes)
         => this with { Scopes = scopes };
+
+    public bool Equals(NewAudience? other)
+    {
+        if (other is null)
+            return false;
+        
+        return Name == other.Name && Scopes.Equals(other.Scopes);
+    }
+    
+    public override int GetHashCode()
+        => HashCode.Combine(Name, Scopes);
 }
