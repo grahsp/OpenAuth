2b0bfb6942384ad6ebb5d5a439793d99add92670
grahsp <pontus.grahsp@gmail.com>
2025-11-15 19:42:54 +0100

    fix: add new methods to client service interface

--- OpenAuth.Application/Clients/Services/IClientService.cs
+++ OpenAuth.Application/Clients/Services/IClientService.cs
@@ -9,6 +9,14 @@ public interface IClientService
     Task<ClientInfo> RenameAsync(ClientId id, ClientName name, CancellationToken ct = default);
     Task DeleteAsync(ClientId id, CancellationToken ct = default);
     
+    Task<ClientDetails> SetGrantTypesAsync(ClientId id, IEnumerable<GrantType> grantTypes, CancellationToken ct = default);
+    Task<ClientDetails> AddGrantTypeAsync(ClientId id, GrantType grantType, CancellationToken ct = default);
+    Task<ClientDetails> RemoveGrantTypeAsync(ClientId id, GrantType grantType, CancellationToken ct = default);
+    
+    Task<ClientDetails> SetRedirectUrisAsync(ClientId id, IEnumerable<RedirectUri> redirectUris, CancellationToken ct = default);
+    Task<ClientDetails> AddRedirectUriAsync(ClientId id, RedirectUri redirectUri, CancellationToken ct = default);
+    Task<ClientDetails> RemoveRedirectUriAsync(ClientId id, RedirectUri redirectUri, CancellationToken ct = default);
+    
     Task<ClientDetails> SetAudiencesAsync(ClientId id, IEnumerable<Audience> audiences, CancellationToken ct = default);
     Task<ClientDetails> AddAudienceAsync(ClientId id, Audience audience, CancellationToken ct = default);
     Task<ClientDetails> RemoveAudienceAsync(ClientId id, AudienceName name, CancellationToken ct = default);
