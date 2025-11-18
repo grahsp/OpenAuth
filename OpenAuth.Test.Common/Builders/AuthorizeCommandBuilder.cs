c0c891b49d36b6ec3aa3bcbc49a2228afe675452
grahsp <pontus.grahsp@gmail.com>
2025-11-18 13:02:57 +0100

    feat: add builder for authorize command object

---
+++ OpenAuth.Test.Common/Builders/AuthorizeCommandBuilder.cs
@@ -0,0 +1,61 @@
+using OpenAuth.Application.OAuth.Authorization.Handlers;
+using OpenAuth.Domain.AuthorizationGrants.ValueObjects;
+using OpenAuth.Domain.Clients.ValueObjects;
+
+namespace OpenAuth.Test.Common.Builders;
+
+public class AuthorizeCommandBuilder
+{
+    private AuthorizeCommand _cmd;
+
+    public AuthorizeCommandBuilder(ClientId clientId, string redirectUri)
+    {
+        _cmd = new AuthorizeCommand
+        (
+            clientId.ToString(),
+            "test-subject",
+            redirectUri,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+    }
+
+    public AuthorizeCommandBuilder WithPkce(Pkce pkce)
+    {
+        _cmd = _cmd with
+        {
+            CodeChallenge = pkce.CodeChallenge,
+            CodeChallengeMethod = pkce.CodeChallengeMethod.ToString()
+        };
+        return this;
+    }
+
+    public AuthorizeCommandBuilder WithAudience(string audience)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(audience);
+        
+        _cmd = _cmd with { Audience = audience };
+        return this;
+    }
+    
+    public AuthorizeCommandBuilder WithScope(string scope)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(scope);
+        
+        _cmd = _cmd with { Scope = scope };
+        return this;
+    }
+
+    public AuthorizeCommandBuilder WithSubject(string subject)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(subject);
+
+        _cmd = _cmd with { Subject = subject };
+        return this;
+    }
+    
+    public AuthorizeCommand Build() => _cmd;
+}
