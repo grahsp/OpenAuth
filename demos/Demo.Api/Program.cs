833873b85d5e9834d5e485f8692006c716ce82e5
grahsp <pontus.grahsp@gmail.com>
2025-12-12 19:06:24 +0100

    feat: initialize mock api for demos

---
+++ demos/Demo.Api/Program.cs
@@ -0,0 +1,34 @@
+namespace Demo.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Configure the HTTP request pipeline.
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                .ToArray();
+            return forecast;
+        });
+
+        app.Run();
+    }
+}
