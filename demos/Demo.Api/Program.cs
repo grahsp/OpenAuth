e177c1c40a2e444927d7fab9eee786630112b1b5
grahsp <pontus.grahsp@gmail.com>
2025-12-12 19:09:00 +0100

    feat: setup authentication for forecast endpoint

--- demos/Demo.Api/Program.cs
+++ demos/Demo.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.IdentityModel.Tokens;
+
 namespace Demo.Api;
 
 public class Program
@@ -6,10 +8,46 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddAuthorization();
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("DemoSpa", policy =>
+            {
+                policy.WithOrigins("http://localhost:5156")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+
+        builder.Services.AddAuthentication("Bearer")
+            .AddJwtBearer("Bearer", options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.Authority = "http://localhost:5067";
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidIssuer = "http://localhost:5067",
+
+                    ValidateAudience = false,
+                    ValidAudience = "weather-api",
+
+                    ValidateLifetime = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+        builder.Services.AddAuthorization();
+
         // Configure the HTTP request pipeline.
         var app = builder.Build();
 
-        app.UseHttpsRedirection();
+        app.UseCors("DemoSpa");
+
+        app.UseAuthentication();
+        app.UseAuthorization();
 
         var summaries = new[]
         {
@@ -17,17 +55,18 @@ public class Program
         };
 
         app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                .ToArray();
-            return forecast;
-        });
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                    .ToArray();
+                return forecast;
+            })
+            .RequireAuthorization();
 
         app.Run();
     }
