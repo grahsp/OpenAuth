12ccb373ec44cfdd647aa45b392a6c1c2a04b2f1
grahsp <pontus.grahsp@gmail.com>
2025-12-12 19:11:38 +0100

    feat: update demo to fetch data from api

--- demos/Notes/Program.cs
+++ demos/Notes/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace Notes;
@@ -11,12 +12,20 @@ public class Program
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-        
-        builder.Logging.SetMinimumLevel(LogLevel.Debug);
-        builder.Logging.AddFilter("Microsoft.AspNetCore.Components.WebAssembly.Authentication", LogLevel.Trace);
-        builder.Logging.AddFilter("Microsoft.AspNetCore.Authorization", LogLevel.Trace);
-        builder.Logging.AddFilter("Microsoft.AspNetCore.Components", LogLevel.Trace);
+        builder.Services.AddHttpClient("WeatherApi", client =>
+            {
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+            })
+            .AddHttpMessageHandler(sp =>
+            {
+                var handler = sp.GetRequiredService<AuthorizationMessageHandler>()
+                    .ConfigureHandler(
+                        authorizedUrls: ["http://localhost:5278"],
+                        scopes: ["weather:read"]
+                    );
+                
+                return handler;
+            });
         
         builder.Services.AddOidcAuthentication(options =>
         {
@@ -26,6 +35,8 @@ public class Program
             options.ProviderOptions.ResponseType = "code";
             options.ProviderOptions.RedirectUri = "authentication/login-callback";
             options.ProviderOptions.MetadataUrl = "http://localhost:5067/.well-known/openid-configuration";
+
+            options.ProviderOptions.DefaultScopes.Add("weather:read");
         });
 
         await builder.Build().RunAsync();
