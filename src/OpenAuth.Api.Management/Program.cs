508e4c8fe44b02af768e89c613b1922f3618c007
grahsp <pontus.grahsp@gmail.com>
2025-12-13 12:50:38 +0100

    feat: initialize new api project for admin dashboard

---
+++ src/OpenAuth.Api.Management/Program.cs
@@ -0,0 +1,25 @@
+namespace OpenAuth.Api.Management;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Add services to the container.
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+
+        // Configure the HTTP request pipeline.
+        var app = builder.Build();
+        
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.Run();
+    }
+}
