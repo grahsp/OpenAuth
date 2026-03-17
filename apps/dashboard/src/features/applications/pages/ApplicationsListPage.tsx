bde15085db01d7748966597fff485c20aaebb91b
grahsp <pontus.grahsp@gmail.com>
2026-03-17 09:10:37 +0100

    feat: add basic behavior using hard coded data

--- apps/dashboard/src/features/applications/pages/ApplicationsListPage.tsx
+++ apps/dashboard/src/features/applications/pages/ApplicationsListPage.tsx
@@ -1,3 +1,24 @@
+import {Link} from "react-router-dom";
+
 export default function ApplicationsListPage() {
-    return <h1>Applications List</h1>;
+    const applications = [
+        { id: 1, name: "App A" },
+        { id: 2, name: "App B" },
+    ];
+
+    return (
+        <div>
+            <h1>Applications</h1>
+
+            <ul>
+                {applications.map(app => (
+                    <li key={app.id}>
+                        <Link to={`/applications/${app.id}`}>
+                            {app.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
