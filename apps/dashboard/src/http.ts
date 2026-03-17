7e0f5f8398878a14a20bdd20b1aa625b7d38952e
grahsp <pontus.grahsp@gmail.com>
2026-03-17 12:10:06 +0100

    feat: improve error handling of http

--- apps/dashboard/src/http.ts
+++ apps/dashboard/src/http.ts
@@ -1,11 +1,39 @@
 import {config} from "./lib/config.ts";
 
 export async function http<T>(path: string, options?: RequestInit): Promise<T> {
-    const res = await fetch(`${config.apiBaseUrl}${path}`, options);
+    const res = await fetch(`${config.apiBaseUrl}${path}`, {
+        headers: {
+            "Content-Type": "application/json",
+            ...options?.headers
+        },
+        ...options
+    });
 
-    if (!res.ok) {
-        throw new Error(`HTTP error: ${res.status}`);
+    let body: unknown;
+
+    try {
+        body = await res.json()
+    } catch {
+        // No JSON body
     }
 
-    return res.json();
+    if (!res.ok) {
+        const errorBody = body as { message?: string, code?: string };
+
+        const code: string | undefined = errorBody?.code;
+        const message = errorBody?.message ?? "Something went wrong";
+
+        throw new ApiError(message, code);
+    }
+
+    return body as T;
+}
+
+export class ApiError extends Error {
+    code?: string;
+
+    constructor(message: string, code?: string) {
+        super(message);
+        this.code = code;
+    }
 }
