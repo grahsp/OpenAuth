d6f415cac4c2e8b8af2c97317702d9c23d4b5a6a
grahsp <pontus.grahsp@gmail.com>
2026-03-17 12:09:46 +0100

    fix: update backend request dto to use camelcase

--- apps/server/OpenAuth.ManagementApi/Clients/ClientCreationRequest.cs
+++ apps/server/OpenAuth.ManagementApi/Clients/ClientCreationRequest.cs
@@ -5,5 +5,5 @@ namespace OpenAuth.ManagementApi.Clients;
 public sealed record ClientCreationRequest(
 	[property: JsonPropertyName("type")] string Type,
 	[property: JsonPropertyName("name")] string Name,
-	[property: JsonPropertyName("redirect_uris")] IEnumerable<string> RedirectUris
+	[property: JsonPropertyName("redirectUris")] IEnumerable<string> RedirectUris
 );
