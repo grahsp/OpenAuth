ffe1a22bb334e6411de5ea853b8521aac5251198
grahsp <pontus.grahsp@gmail.com>
2026-03-22 20:07:46 +0100

    feat: add composite hook for fetching apis with client-api access

---
+++ apps/dashboard/src/features/applications/details/hooks/useClientPermissions.tsx
@@ -0,0 +1,34 @@
+import {useClientApiAccess} from "./useClientApiAccess.tsx";
+import {useMemo} from "react";
+import {useApis} from "../../hooks.tsx";
+
+export function useClientPermissions(clientId?: string) {
+    const apis = useApis();
+    const access = useClientApiAccess(clientId);
+
+    const data = useMemo(() => {
+        const apisData = apis.data;
+        const accessData = access.data
+
+        if (!apisData || !accessData)
+            return [];
+
+        return apisData.map(api => {
+            const match = accessData.find(a => a.id === api.id);
+
+            return {
+                apiId: api.id,
+                name: api.name,
+                authorized: !!match,
+                availableScopes: api.permissions.map(p => ({
+                    label: p.scope,
+                    value: p.scope,
+                    description: p.description
+                })),
+                selectedScopes: match?.scopes ?? []
+            }
+        })
+    }, [apis.data, access.data]);
+
+    return {data, loading: apis.loading || access.loading, error: apis.error || access.error};
+}
