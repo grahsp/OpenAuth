7a6704754cf4158bbb42ee0e6a3ed1597278f2ac
grahsp <pontus.grahsp@gmail.com>
2026-03-27 21:25:59 +0100

    feat: add overload method for ApiResourceNotFoundException accepting audience

--- apps/server/src/OpenAuth.Application/ApiResources/ApiResourceNotFoundException.cs
+++ apps/server/src/OpenAuth.Application/ApiResources/ApiResourceNotFoundException.cs
@@ -3,4 +3,13 @@ using OpenAuth.Domain.ApiResources.ValueObjects;
 
 namespace OpenAuth.Application.ApiResources;
 
-public sealed class ApiResourceNotFoundException(ApiResourceId apiResourceId) : NotFoundException($"API resource with ID '{apiResourceId}' not found.");
+public sealed class ApiResourceNotFoundException : NotFoundException
+{
+	public ApiResourceNotFoundException(ApiResourceId apiResourceId)
+		: base($"API resource with ID '{apiResourceId}' not found.")
+	{}
+
+	public ApiResourceNotFoundException(AudienceIdentifier audience)
+		: base($"API resource with Audience '${audience}' not found.")
+	{}
+}
